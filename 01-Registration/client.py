ee0477a5dc370694f76b57192f410d0cfd0ef065
Michał Grabowski <m.grabowski@avsystem.com>
2024-01-21 20:19:13 +0100

    Adds deregister operation

--- 01-Registration/client.py
+++ 01-Registration/client.py
@@ -1,17 +1,34 @@
 import asyncio
-from aiocoap import Context,Message, POST
+from aiocoap import Context, Message, POST, DELETE
 
 endpoint_name = "urn:nai:no-sec@mikegpl.dev"
 
 server_url = "coap://eu.iot.avsystem.cloud:5683"
 
+
 def make_register_path(device_name, lifetime_seconds="60", lwm2m_version="1.2", binding="U"):
     return f"/rd?ep={device_name}&lt={lifetime_seconds}&lwm2m={lwm2m_version}&b={binding}&Q"
 
+
 def make_register_message(server_addr, device_name, lifetime_seconds="60", lwm2m_version="1.2", binding="U"):
-    path = make_register_path(device_name, lifetime_seconds, lwm2m_version, binding)
+    path = make_register_path(
+        device_name, lifetime_seconds, lwm2m_version, binding)
     msg = Message(
         code=POST,
+        uri=f"{server_addr}{path}"
+    )
+    return msg
+
+
+def make_deregister_path(device_location_parts):
+    assert len(device_location_parts) > 1
+    return "/" + "/".join(device_location_parts)
+
+
+def make_deregister_message(server_addr, device_location_parts):
+    path = make_deregister_path(device_location_parts)
+    msg = Message(
+        code=DELETE,
         uri=f"{server_addr}{path}"
     )
     return msg
@@ -28,15 +45,21 @@ async def register(coap_client, server, device_name):
     response = await coap_client.request(register_message).response
     return response, response_options_as_list(response)
 
+
 async def main():
     try:
         coap_client = await Context.create_client_context()
         print("Making request to server to register device")
-        register_response = await register(coap_client, server_url, endpoint_name)
+        (_, device_location_parts) = await register(coap_client, server_url, endpoint_name)
+        print(f"Deregistering device at location {device_location_parts}")
+        deregister_response = await coap_client.request(make_deregister_message(server_url, device_location_parts)).response
+        print(f"Response for deregister operation: {deregister_response.code}\n{deregister_response.opt}\n{deregister_response.payload}")
     except Exception as e:
-        print(f"Failed to register/deregister device in LwM2M server at {server_url}")
+        print(
+            f"Failed to register/deregister device in LwM2M server at {server_url}")
         print(e)
     else:
-        print(f"Response from server: {register_response[0].code}\n{register_response[1]}")
+        print(f"Succesfully completed device operations")
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
--- 01-Registration/tests.py
+++ 01-Registration/tests.py
@@ -1,5 +1,5 @@
-from client import make_register_path, make_register_message
-from aiocoap import POST
+from client import make_register_path, make_register_message, make_deregister_path, make_deregister_message
+from aiocoap import POST, DELETE
 
 
 def test_register_path():
@@ -15,12 +15,30 @@ def test_register_message():
         device_name="urn:nai:no-sec@mikegpl.dev",
         lifetime_seconds="60",
         lwm2m_version="1.2",
-        binding="U") 
-    
-    assert msg.code == POST 
-    assert "coap://eu.iot.avsystem.clsoud" in msg.get_request_uri(), f"Server should be included in request URI: {msg.get_request_uri()}"
+        binding="U")
+
+    assert msg.code == POST
+    assert "coap://eu.iot.avsystem.cloud" in msg.get_request_uri(
+    ), f"Server should be included in request URI: {msg.get_request_uri()}"
+
+
+def test_deregister_path():
+    assert make_deregister_path(device_location_parts=[
+                                'rd', 'urn:nai:no-sec@mikegpl.dev']) == '/rd/urn:nai:no-sec@mikegpl.dev', "Deregister path for COAP transport should follow 6.4.3 section of https://openmobilealliance.org/RELEASE/LightweightM2M/V1_1-20180612-C/OMA-TS-LightweightM2M_Transport-V1_1-20180612-C.pdf"
+
+
+def test_deregister_message():
+    msg = make_deregister_message(
+        server_addr="coap://eu.iot.avsystem.cloud",
+        device_location_parts=['rd', 'urn:nai:no-sec@mikegpl.dev']
+    ) 
+    assert msg.code == DELETE
+    assert "rd" in msg.get_request_uri(), f"Device location prefix should be included in request URI: {msg.get_request_uri()}"
+    assert "coap://eu.iot.avsystem.cloud" in msg.get_request_uri(), f"Server should be included in request URI: {msg.get_request_uri()}"
+
 
 test_register_path()
 test_register_message()
+test_deregister_path()
 
 print("Tests passed")
